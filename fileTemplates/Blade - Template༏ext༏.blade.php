477b0aef379cd71efd38c93298c827cf8ecdd046
Nathan Lochala <rnlochala@alaska.edu>
2019-04-10 11:05:43 +0800

    PS-2019.1 <rnlochala@Nathans-MacBook-Pro.local Create baseRefactoring.xml, markdown.xml, vim_settings.xml, _@user_Bold Contrast _rainglow_.icls, ide.general.xml, _@user_Hub Contrast _rainglow_.icls, composerJson.xml, _@user_Jumper Contrast _rainglow_.icls, project.default.xml, Default copy.xml, _@user_Laracasts Contrast _rainglow_.icls, terminal.xml, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, php.xml, colors.scheme.xml, _@user_Darcula.icls, _@user_Gloom Contrast _rainglow_.icls, Blade - Template༏ext༏.blade.php, _@user_Brave Contrast _rainglow_.icls, _@user_Material Darker.icls, material_custom_theme.xml, Blade - Empty༏ext༏.blade.php, laf.xml, _@user_Box UK Contrast _rainglow_.icls, macros.xml, material_theme.xml, _@user_Blink Contrast _rainglow_.icls, keymap.xml, editor.xml, webServers.xml, Blackboard - Portal.icls, debugger.xml, updates.xml

---
+++ fileTemplates/Blade - Template༏ext༏.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.backend')
+
+@section('content')
+<!-- Add Content Title Here b.breadcrumbs -->
+    @include('layouts._content_start')
+<!--
+    panel.row
+    panel.column
+    panel.panel
+    panel.panel
+
+    ---------------
+    panel.row
+    panel.column
+    panel.panel
+    panel.column
+    panel.panel
+
+    |--------------||--------------|
+    |              ||              |
+    |--------------||--------------|
+
+-->
+
+    @include('layouts._content_end')
+@endsection
